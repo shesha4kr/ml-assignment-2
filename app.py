2f081fd6f8d23022e2630531321e975c02a07aa3
shesha4kr <shesha4kr@gmail.com>
2026-01-10 21:34:27 +0530

    added logic to read CSV file and preprocess the data

--- app.py
+++ app.py
@@ -1,11 +1,59 @@
 import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
 
 st.set_page_config(page_title="My First Streamlit App", layout="centered")
 
 st.title("🫀 Heart Disease Risk Prediction")
 st.subheader("Different ML Model Performance Comparison")
 
+# define features with numerical and categorical values
+numerical_cols = [ 'age', 'resting_blood_pressure', 'cholestoral', 'Max_heart_rate', 'oldpeak' ]
+categorical_cols = [ 'sex', 'chest_pain_type', 'fasting_blood_sugar', 'rest_ecg', 'exercise_induced_angina', 'slope', 'vessels_colored_by_flourosopy', 'thalassemia' ]
 
+# load CSV file uploaded in google drive
+@st.cache_data
+def load_data():
+    file_id = "1AnHrT9-13EI9lJgCg-6i_A-YQVP2dTka"
+    url = f"https://drive.google.com/uc?id={file_id}&export=download"
+    df = pd.read_csv(url)
+    # Shuffle the data
+    df = df.sample(frac=1, random_state=42).reset_index(drop=True)
+    return df
+
+# define preprocessor to scale and encode the features and split the dataset
+@st.cache_data
+def prepare_data(df):
+    TARGET_COLUMN = "target"
+    X = df.drop(columns=[TARGET_COLUMN])
+    y = df[TARGET_COLUMN]
+
+    # Preprocessing
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("num", StandardScaler(), numerical_cols),
+            ("cat", OneHotEncoder(handle_unknown="ignore", sparse_output=False), categorical_cols)
+        ]
+    )
+
+    # Train-test split (VERY IMPORTANT: stratify)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size=0.2,
+        random_state=42,
+        stratify=y
+    )
+
+    return X_train, X_test, y_train, y_test, preprocessor
+
+df = load_data()
+X_train, X_test, y_train, y_test, preprocessor = prepare_data(df)
+
+# Dropdowns
 model_choice = st.selectbox(
     "Select a Machine Learning Model",
     (
@@ -42,10 +90,7 @@ def xgboost_model():
     st.subheader("XGBoost Classifier")
     st.write("Running XGBoost model...")
 
-# ----------------------------
-# Dispatcher (Clean & Scalable)
-# ----------------------------
-
+# dropdown dispatcher
 model_dispatcher = {
     "Logistic Regression": logistic_regression,
     "Decision Tree Classifier": decision_tree,
