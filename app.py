6247a610107e8e4f9126e2ee49f4b20d4efbd35f
shesha4kr <shesha4kr@gmail.com>
2026-01-10 08:56:11 +0530

    Initial streamlit commit

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="My First Streamlit App", layout="centered")
+
+st.title("🫀 Heart Disease Risk Prediction")
+st.subheader("Different ML Model Performance Comparison")
